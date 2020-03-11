6c5b1881e2b394201784c96ac645f9ae1b093f53
RyanWen <apg9528221@hotmail.com>
2020-03-11 21:23:59 +0800

    Add files via upload

---
+++ FF_GAMEDlg.h
@@ -0,0 +1,51 @@
+﻿
+// FF_GAMEDlg.h: 標頭檔
+//
+#include"pch.h"
+#pragma once
+
+
+// CFFGAMEDlg 對話方塊
+class CFFGAMEDlg : public CDialogEx
+{
+// 建構
+public:
+	CFFGAMEDlg(CWnd* pParent = nullptr);	// 標準建構函式
+
+// 對話方塊資料
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_FF_GAME_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支援
+
+
+// 程式碼實作
+protected:
+	HICON m_hIcon;
+
+	// 產生的訊息對應函式
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+
+	afx_msg void OnBnClickedRecord();
+	void OnBnClickedRecordAudio();
+	void Stop();
+	//====================================================================================================================
+//錄音
+	bool win;//采集數據標志位
+	FILE* f;//存儲采集到的音頻文件
+	HWAVEIN hWaveIn;  //輸入設備
+	WAVEFORMATEX waveform; //采集音頻的格式，結構體
+	BYTE* pBuffer1, * pBuffer2;//采集音頻時的數據緩存
+	WAVEHDR wHdr1, wHdr2; //采集音頻時包含數據緩存的結構體
+	static DWORD CALLBACK MicCallback(HWAVEIN hWaveIn, UINT uMsg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2);//消息
+	//====================================================================================================================
+	CWinThread* m_pthreadVideo;
+	static UINT __cdecl  VideoPlay(LPVOID lparam);
+};
---
+++ pch.h
@@ -0,0 +1,127 @@
+﻿// pch.h: 此為先行編譯的標頭檔。
+// 以下所列檔案只會編譯一次，可改善之後組建的組建效能。
+// 這也會影響 IntelliSense 效能，包括程式碼完成以及許多程式碼瀏覽功能。
+// 但此處所列的檔案，如果其中任一在組建之間進行了更新，即會重新編譯所有檔案。
+// 請勿於此處新增會經常更新的檔案，如此將會對於效能優勢產生負面的影響。
+
+#ifndef PCH_H
+#define PCH_H
+
+// 請於此新增您要先行編譯的標頭
+#include "framework.h"
+
+#endif //PCH_H
+//===================================
+//錄音
+#include <SDKDDKVer.h>
+/*
+#include<windows.h>
+#include <MMSYSTEM.H>
+#pragma comment(lib, "WINMM.LIB")
+*/
+#include <Windows.h>
+#pragma comment(lib, "Winmm.lib")
+#include <dshow.h>
+#pragma comment(lib, "Strmiids.lib")
+//===================================
+
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavutil/avutil.h>
+#include <libavformat/avformat.h>
+#include <libavformat/avio.h>
+#include <libswscale/swscale.h>
+#include <libavdevice/avdevice.h>
+#include <libavformat/avformat.h>
+#include <libavutil/mathematics.h>
+#include <libavutil/time.h>
+#include <libswresample/swresample.h>
+#include <libavfilter/buffersink.h>
+#include<libavfilter/buffersrc.h>
+#include<libavutil/opt.h>
+
+#include <libavutil/avassert.h>
+#include <libavutil/channel_layout.h>
+#include <libavutil/opt.h>
+#include <libavutil/mathematics.h>
+#include <libavutil/timestamp.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libswresample/swresample.h>
+
+
+
+#include <libavutil/avstring.h>
+#include <libavutil/mathematics.h>
+#include <libavutil/pixdesc.h>
+#include <libavutil/imgutils.h>
+#include <libavutil/dict.h>
+#include <libavutil/parseutils.h>
+#include <libavutil/samplefmt.h>
+#include <libavutil/avassert.h>
+#include <libavutil/time.h>
+
+#include <libavformat/avformat.h>
+#include <libavdevice/avdevice.h>
+#include <libswscale/swscale.h>
+#include <libavutil/opt.h>
+#include <libavcodec/avfft.h>
+#include <libswresample/swresample.h>
+
+#include <libavcodec/avcodec.h>
+#include <libavfilter/buffersink.h>
+#include <libavfilter/buffersrc.h>
+#include <libavutil/channel_layout.h>
+#include <libavutil/common.h>
+#include <libavutil/hwcontext.h>
+#include "libavcodec/avcodec.h"  
+#include "libavformat/avformat.h"  
+#include "libswscale/swscale.h"  
+#include "libavdevice/avdevice.h"  
+#include "libavutil/audio_fifo.h"  
+}
+
+
+#ifdef _DEBUG
+#pragma comment(lib,"OpenCV/opencv_calib3d2413d.lib")
+#pragma comment(lib,"OpenCV/opencv_contrib2413d.lib")
+#pragma comment(lib,"OpenCV/opencv_core2413d.lib")
+#pragma comment(lib,"OpenCV/opencv_features2d2413d.lib")
+#pragma comment(lib,"OpenCV/opencv_flann2413d.lib")
+#pragma comment(lib,"OpenCV/opencv_gpu2413d.lib")
+#pragma comment(lib,"OpenCV/opencv_highgui2413d.lib")
+#pragma comment(lib,"OpenCV/opencv_imgproc2413d.lib")
+#pragma comment(lib,"OpenCV/opencv_legacy2413d.lib")
+#pragma comment(lib,"OpenCV/opencv_ml2413d.lib")
+#pragma comment(lib,"OpenCV/opencv_nonfree2413d.lib")
+#pragma comment(lib,"OpenCV/opencv_objdetect2413d.lib")
+#pragma comment(lib,"OpenCV/opencv_ocl2413d.lib")
+#pragma comment(lib,"OpenCV/opencv_photo2413d.lib")
+#pragma comment(lib,"OpenCV/opencv_stitching2413d.lib")
+#pragma comment(lib,"OpenCV/opencv_superres2413d.lib")
+#pragma comment(lib,"OpenCV/opencv_ts2413d.lib")
+#pragma comment(lib,"OpenCV/opencv_video2413d.lib")
+#pragma comment(lib,"OpenCV/opencv_videostab2413d.lib")
+#else
+#pragma comment(lib,"OpenCV/opencv_calib3d2411.lib")
+#pragma comment(lib,"OpenCV/opencv_contrib2411.lib")
+#pragma comment(lib,"OpenCV/opencv_core2411.lib")
+#pragma comment(lib,"OpenCV/opencv_features2d2411.lib")
+#pragma comment(lib,"OpenCV/opencv_flann2411.lib")
+#pragma comment(lib,"OpenCV/opencv_gpu2411.lib")
+#pragma comment(lib,"OpenCV/opencv_highgui2411.lib")
+#pragma comment(lib,"OpenCV/opencv_imgproc2411.lib")
+#pragma comment(lib,"OpenCV/opencv_legacy2411.lib")
+#pragma comment(lib,"OpenCV/opencv_ml2411.lib")
+#pragma comment(lib,"OpenCV/opencv_nonfree2411.lib")
+#pragma comment(lib,"OpenCV/opencv_objdetect2411.lib")
+#pragma comment(lib,"OpenCV/opencv_ocl2411.lib")
+#pragma comment(lib,"OpenCV/opencv_photo2411.lib")
+#pragma comment(lib,"OpenCV/opencv_stitching2411.lib")
+#pragma comment(lib,"OpenCV/opencv_superres2411.lib")
+#pragma comment(lib,"OpenCV/opencv_ts2411.lib")
+#pragma comment(lib,"OpenCV/opencv_video2411.lib")
+#pragma comment(lib,"OpenCV/opencv_videostab2411.lib")
+
+#endif
